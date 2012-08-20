a6666bdef45d800c785da95a78e00318ba1f62d6
Jenkins <jenk@tapsquare.com>
2012-08-19 19:53:15 -0700

    build it

--- JenkinsIOSExample/JenkinsIOSExampleAppDelegate.m
+++ JenkinsIOSExample/JenkinsIOSExampleAppDelegate.m
@@ -23,7 +23,7 @@
      
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
-    NSLog(@"open wide duede");
+    NSLog(@"open wide dude");
     return YES;
 }
 
